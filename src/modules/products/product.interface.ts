71e7acfdb3e7bca4a67708870fdf4a4c2bb22cbd
istiakantu <istakahmed636@gmail.com>
2024-07-09 21:39:16 +0600

    interface added

---
+++ src/modules/products/product.interface.ts
@@ -0,0 +1,21 @@
+type Variant = {
+  type: string;
+  value: string;
+};
+
+type Inventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+type Product = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: Variant[];
+  inventory: Inventory;
+};
+
+export default Product;
