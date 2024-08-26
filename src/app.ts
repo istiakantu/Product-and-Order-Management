c92c3185a45d5b0b8dfee4892d508f4a02979b16
istiakantu <istakahmed636@gmail.com>
2024-08-26 22:32:44 +0600

    catch all not found route

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,14 @@ app.use(express.json());
 app.use("/api/products", ProductRoutes);
 app.use("/api/orders", OrderRoutes);
 
+// Catch all "Not Found" route
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next!");
 });
